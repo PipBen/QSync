88f22215f99107ab12d4241ab523528ac156d875
c-nation <c.nation@ucl.ac.uk>
2020-09-28 15:31:57 +0100

    Initial Commit of codes. Dimer_Model_Detune_1 is the full code DimerDetuneNew is a version that I (Charlie) have re-written - it is not complete, but may be easier to follow for the parts that are done.

---
+++ DimerDetuneNew.py
@@ -0,0 +1,232 @@
+
+"""
+Code for two coupled dimer chromophores, strongly coupled to modes.
+Tensor order is excitonic, mode 1, mode 2.
+@author: Charlie Nation
+based on code by stefan siwiak-jaszek
+
+___________________________________________________________________________________________________________________
+
+To do:
+
+Replace np.matrix (depreciated) with np.array throughout - this doesn't have a getH method, so will need
+.conj().transpose() I think
+
+"""
+import numpy as np
+import scipy.constants as constant
+import scipy.integrate as integrate
+import time
+import scipy.sparse as sp
+import scipy.linalg as sl
+
+
+class DimerDetune:
+
+    def __init__(self, n_vib_cutoff=5, temperature=298):
+        self.omega = 1111
+        self.huang = 0.0578
+        self.detuning = 1.00
+        self.w1 = self.omega
+        self.w2 = self.detuning * self.omega
+        self.e1 = 0 + self.w1 * self.huang
+        self.e2 = 1042 + self.w2 * self.huang  # 946.412
+        self.de = self.e2 - self.e1
+        self.V = 92  # 236.603 # cm-1
+        self.dE = np.sqrt(self.de ** 2 + 4 * self.V ** 2)  # energy between excitonic states
+        self.theta = 0.5 * np.arctan(2 * self.V / self.de)
+        self.a = 1
+        self.g1 = self.w1 * np.sqrt(self.huang) * self.a
+        self.g2 = self.w2 * np.sqrt(self.huang) * self.a
+
+        self.kBT = (constant.k * temperature) / (constant.h * constant.c * 100)  # cm-1
+        self.thermal_dissipation = 33.3564  # 70
+        self.electronic_dephasing = 333.564
+        self.taudiss = 1 / (1e-12 * self.thermal_dissipation * 100 * constant.c)
+        self.taudeph = 1 / (1e-12 * self.electronic_dephasing * 100 * constant.c)
+        self.N = n_vib_cutoff  # 7
+
+        self.dimH = 2 * n_vib_cutoff**2  # Hilbert space dimension
+
+        # %% scaling effects from setting 2pi x c = 1 and hbar = 1
+
+        self.r_el = self.electronic_dephasing / (2 * constant.pi)
+        self.r_th = self.thermal_dissipation / (2 * constant.pi)
+        self.r_v1 = self.r_th  # 6 # cm-1
+        self.r_v2 = self.r_th  # 6 # cm-1
+
+        # Exciton Vectors
+
+        self.E1 = sp.lil_matrix(np.matrix([[1.], [0.]])).tocsr()
+        self.E2 = sp.lil_matrix(np.matrix([[0.], [1.]])).tocsr()
+
+    def exciton_operator(self, e1, e2):
+        """returns |E_e1><E_e2|"""
+        if e1 == 1:
+            e_1 = self.E1
+        elif e1 == 2:
+            e_1 = self.E2
+        else:
+            raise Exception('e1 should be 1 or 2 for |E_1> or |E_2>')
+        if e2 == 1:
+            e_2 = self.E1
+        elif e2 == 2:
+            e_2 = self.E2
+        else:
+            raise Exception('e2 should be 1 or 2 for <E_1| or <E_2|')
+        return sp.kron(e_1, e_2.getH())
+
+    def rotate(self):
+        """Unitary rotation from site to excitonic bases"""
+        return sp.lil_matrix(np.matrix([[np.cos(self.theta), np.sin(self.theta)],
+                                        [-np.sin(self.theta), np.cos(self.theta)]])).tocsr()
+
+    def vib_mode_operator(self, k, j):
+        """returns |k><j| in the vibrational Fock space"""
+
+        mode1 = np.matrix(np.zeros([self.N, 1]))
+        mode2 = np.matrix(np.zeros([self.N, 1]))
+        mode1[k, 0] = 1
+        mode2[j, 0] = 1
+
+    def destroy(self):
+        """Annihilation operator on vibrational Fock space"""
+        return sp.diags(np.sqrt(np.arange(1, self.N)), 1).tocsr()
+
+    def identity_vib(self):
+        """identity operator on vibrational Fock space"""
+        return sp.eye(self.N, self.N).tocsr()
+
+    def thermal(self, w):
+        """Thermal density operator on vibrational Fock space"""
+        p = np.matrix(np.zeros((self.N, self.N)))
+        for i in np.arange(self.N):
+            p[i, i] = (1 - np.exp(-w / self.kBT)) * np.exp(-w * i / self.kBT)
+        return sp.lil_matrix(p).tocsr()
+
+    def a_k(self, k):
+        return self.rotate() * self.exciton_operator(k, k) * self.rotate().getH()
+
+    @staticmethod
+    def dissipator(operator, rate):
+        """Dissipator in Liouville space"""
+        d = rate * (sp.kron(operator, operator.conj()).tocsr()
+                    - 0.5 * sp.kron(operator.getH() * operator, sp.eye(operator.shape[1])).tocsr()
+                    - 0.5 * sp.kron(sp.eye(operator.shape[1]), (operator.getH() * operator).transpose()).tocsr())
+        return d
+
+    @staticmethod
+    def liouv_commutator(operator):
+        """Commutatior in Liouville space"""
+        h = sp.kron(operator, sp.eye(operator.shape[1])).tocsr() - sp.kron(sp.eye(operator.shape[1]), operator.transpose()).tocsr()
+        return h
+
+    def hamiltonian(self):
+        """dE/2 sigma_z + omega_1 n_1 + omega_2 n_2 + g_1 |1><1| (b_1 + b^dag_1) + g_2 |2><2| (b_2 + b^dag_2 )
+        |1(2)><1(2)| is the site basis ground (excited) state"""
+        oE1 = self.exciton_operator(1, 1)
+        oE2 = self.exciton_operator(2, 2)
+        sigmaz = oE2 - oE1   # note that this is backwards... redefine it when going back over this
+        b = self.destroy()
+        Iv = self.identity_vib()
+        eldis1 = self.a_k(1)
+        eldis2 = self.a_k(2)
+        Ie = sp.eye(2, 2).tocsr()
+
+        H = sp.kron((self.dE / 2) * sigmaz, sp.kron(Iv, Iv)) \
+            + sp.kron(Ie, sp.kron(self.w1 * b.getH() * b, Iv)) \
+            + sp.kron(Ie, sp.kron(Iv, self.w2 * b.getH() * b)) \
+            + self.g1 * sp.kron(eldis1, sp.kron((b + b.getH()), Iv)) \
+            + self.g2 * sp.kron(eldis2, sp.kron(Iv, (b + b.getH())))
+
+        return H
+
+    def liouvillian(self):
+        H = self.hamiltonian()
+        Iv = self.identity_vib()
+        Ie = sp.eye(2, 2).tocsr()
+        ELdis1 = sp.kron(self.a_k(1), sp.kron(Iv, Iv)).tocsr()
+        ELdis2 = sp.kron(self.a_k(1), sp.kron(Iv, Iv)).tocsr()
+        b = self.destroy()
+        oB1 = sp.kron(Ie, sp.kron(b, Iv)).tocsr()
+        oB2 = sp.kron(Ie, sp.kron(Iv, b)).tocsr()
+        nw1 = 1 / (np.exp(self.w1 / self.kBT) - 1)  # thermal distribution
+        nw2 = 1 / (np.exp(self.w2 / self.kBT) - 1)
+
+        L = -1j * self.liouv_commutator(H) + self.dissipator(ELdis1, self.r_el) + self.dissipator(ELdis2, self.r_el) \
+            + self.dissipator(oB1, self.r_v1 * (nw1 + 1)) + self.dissipator(oB1.getH(), self.r_v1 * nw1) \
+            + self.dissipator(oB2, self.r_v2 * (nw2 + 1)) + self.dissipator(oB2.getH(), self.r_v2 * nw2)
+        return L
+
+    def init_state(self):
+        oE2 = self.exciton_operator(2, 2)
+        return sp.kron(oE2, sp.kron(self.thermal(self.w1), self.thermal(self.w2))).todense()
+
+
+class Operations(DimerDetune):
+    """input class dimer needs:
+    Methods:
+        liouvillian
+        hamiltonian
+        init_state
+    Arguments:
+        dimH - Hilbert space dimenstion
+        omega - some frequency of interest to be resolved by time step
+        """
+
+    def steady_state(self):
+        l = self.liouvillian()
+        a = sl.eig(l)
+        eners = a[0]
+        eigstates = a[1]
+        print('max eigenvalue is ', max(np.real(eners)))
+        max_index = np.argmax(eners)
+        rho_ss = eigstates[max_index, :].reshape([self.dimH, self.dimH])
+        rho_ss = rho_ss / np.trace(rho_ss)
+        return rho_ss
+
+    def time_evol_me(self, t0, tmax_ps, dt=None):
+        count1 = time.time()
+        # time setup for integration
+        if dt is None:
+            dt = ((2 * constant.pi) / self.omega) / 100
+        tmax = tmax_ps * 100 * constant.c * 2 * constant.pi * 1e-12
+        steps = np.int((tmax - t0) / dt)  # total number of steps. Must be int.
+
+        # initial state
+        rho0 = self.init_state()
+        rho0_l = rho0.reshape(1, (rho0.shape[1]) ** 2)  # initial state in Liouville space
+
+        # function to be integrated
+        L = self.liouvillian()
+
+        def f(t, y):
+            pt = L.dot(y.transpose()).transpose()  # Master equation in Liouville space
+            return pt
+
+        evo = integrate.complex_ode(f)
+        evo.set_initial_value(rho0_l, t0)  # initial conditions
+        t = np.zeros(steps)
+
+        t[0] = t0
+        rhoT = np.zeros((steps, rho0.shape[1] ** 2), dtype=complex)  # time is 3rd dim.
+        rhoT[0, :] = rho0_l
+        # now do the iteration.
+        k = 1
+        while evo.successful() and k < steps:
+            evo.integrate(evo.t + dt)  # time to integrate at at each loop
+            t[k] = evo.t  # save current loop time
+            rhoT[k, :] = evo.y  # save current loop data
+            k += 1  # keep index increasing with time.
+        count2 = time.time()
+        print('Integration =', count2 - count1)
+
+        return rhoT
+
+
+if __name__ == "__main__":
+    n_cutoff = 3
+    dimer = DimerDetune(n_cutoff)
+    ops = Operations()
+    rho0 = ops.steady_state()
+
